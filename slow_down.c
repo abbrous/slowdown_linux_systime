bc3f57aee5681e6b364486c1ab4a5f962e3985dc
weiwen zhong <azhongweiwen@sina.com>
2018-08-26 09:03:30 +0800

    Update slow_down.c

--- slow_down.c
+++ slow_down.c
@@ -7,27 +7,28 @@
 // Use at your own risk
 // 10 seconds to run, time slow down 1 seconds
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     struct timeval tv;
     const struct timespec ts = {0,100000000};//0.1s
     int ret = 0;
     int i = 0;
 
-    for (i=0; i< 100; i++) { //running 0.1s*100=10s
+    for (i=0; i< 100; i++) { 
         gettimeofday(&tv, NULL);
-	nanosleep(&ts, NULL);
-	if (tv.tv_usec < 990000) {
-		tv.tv_usec += 10000;//0.01s
-	} else if (tv.tv_usec >= 990000) {
-		tv.tv_usec -= 990000;
-		tv.tv_sec += 1;
-	}
+        nanosleep(&ts, NULL); 
+        if (tv.tv_usec < 910000) {
+                tv.tv_usec += 90000; //every 0.1s step 0.09s, time leak = 0.1s
+        } else if (tv.tv_usec >= 910000) {
+                tv.tv_usec -= 910000;
+                tv.tv_sec += 1;
+        }
         ret = settimeofday(&tv, NULL);
-	if (ret != 0) {
-		perror("something went wrong!");
-	}
+        if (ret != 0) {
+                perror("something went wrong!");
+        }
     }
 
     return 0;
 }
+
